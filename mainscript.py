20a954806283c7e736786def9556053ecaa1646e
Rutvik Lhase <Rutvik.Lhase@genmills.com>
2023-02-02 23:32:16 +0530

    Edited workflow

--- mainscript.py
+++ mainscript.py
@@ -1,4 +1,5 @@
 from git.repo.base import Repo
 import os
 Repo.clone_from("https://github.com/rutviklhase/headout_repo.git", "./repo")
+os.system('sudo docker run 705706633861.dkr.ecr.us-east-2.amazonaws.com/headout_assignment2:headout_tag')
 os.system('node ./repo/build/libs/project.js')
